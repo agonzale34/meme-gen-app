0b4f5e98a4450077380fc3a949dc2b2e1fc292cc
Anthony Gonzalez <agonzalez@lixar.com>
2020-07-29 17:13:32 -0300

    adding basic layout

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry, StatusBar } from 'react-native';
 import {name as appName} from './app.json';
+import {
+  Appbar,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import HomeScreen from './src/containers/HomeScreen';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => HomeScreen);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3f51b5',
+    accent: '#ab47bc',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <StatusBar backgroundColor="#002984" />
+      <Appbar>
+        <Appbar.Action icon="emoticon-tongue-outline" onPress={() => {}} />
+        <Appbar.Content title="Meme Generator" />
+        <Appbar.Action icon="dots-vertical" onPress={() => {}} />
+      </Appbar>
+      <HomeScreen />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
--- src/containers/HomeScreen.tsx
+++ src/containers/HomeScreen.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {Button, Card, TextInput} from 'react-native-paper';
+import {ScrollView, View} from 'react-native';
+import {withTheme} from 'react-native-paper';
 
-const HomeScreen = () => {
+const HomeScreen = (props: any) => {
+  const {colors} = props.theme;
   return (
-    <>
-      <StatusBar backgroundColor="#3f51b5" />
-    </>
+    <ScrollView>
+      <View style={{padding: 10}}>
+        <Card>
+          <Card.Actions>
+            <Button mode="contained" style={{backgroundColor: colors.accent}}>
+              RANDOM IMAGE
+            </Button>
+          </Card.Actions>
+          <Card.Cover source={{uri: 'http://i.imgflip.com/1bij.jpg'}} />
+          <Card.Content>
+            <TextInput mode="outlined" label="Top Text" />
+            <TextInput mode="outlined" label="Bottom Text" />
+          </Card.Content>
+        </Card>
+      </View>
+    </ScrollView>
   );
 };
 
-export default HomeScreen;
+export default withTheme(HomeScreen);
