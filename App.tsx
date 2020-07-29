351c8ae9a952c742d30533248d7520ef80c65826
Anthony Gonzalez <agonzalez@lixar.com>
2020-07-29 15:12:15 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#3f51b5" />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ src/containers/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#3f51b5" />
+    </>
+  );
+};
+
+export default HomeScreen;
