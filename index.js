19e3ac5b4f3f77fcb5fb5da3c45a5c217d59dae2
Anthony Gonzalez <agonzalez@lixar.com>
2020-08-06 15:02:02 -0300

    Ready to be publish

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import { AppRegistry, StatusBar } from 'react-native';
+import {AppRegistry, StatusBar} from 'react-native';
 import {name as appName} from './app.json';
 import {
   Appbar,
+  Button,
   DefaultTheme,
+  Dialog,
+  Paragraph,
+  Portal,
   Provider as PaperProvider,
 } from 'react-native-paper';
 import HomeScreen from './src/containers/HomeScreen';
@@ -18,14 +22,34 @@ const theme = {
 };
 
 export default function Main() {
+  const [visible, setVisible] = React.useState(false);
+
   return (
     <PaperProvider theme={theme}>
       <StatusBar backgroundColor="#002984" />
-      <Appbar>
-        <Appbar.Action icon="emoticon-tongue-outline" onPress={() => {}} />
+      <Appbar.Header>
+        <Appbar.Action
+          icon="emoticon-tongue-outline"
+          onPress={() => setVisible(true)}
+        />
         <Appbar.Content title="Meme Generator" />
         <Appbar.Action icon="dots-vertical" onPress={() => {}} />
-      </Appbar>
+      </Appbar.Header>
+      <Portal>
+        <Dialog visible={visible} onDismiss={() => setVisible(false)}>
+          <Dialog.Title>Information</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>
+              App developed with React Native
+              {'\n'}
+              Developer: Anthony Gonzalez
+            </Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => setVisible(false)}>OK</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
       <HomeScreen />
     </PaperProvider>
   );
